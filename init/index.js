700572e4ade65529e1f7723ae43162366e9b4d5f
Visk4 <144266579+Visk4@users.noreply.github.com>
2024-08-17 12:45:07 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -2,16 +2,16 @@ const mongoose = require("mongoose");
 const Listing = require("../models/listing.js");
 const initData = require("./data.js");
 
-async function main() {
-    await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
-}
-main().then(()=>{
-    console.log("connected to DB");
-}).catch((err)=>{
-    console.log(err);
-})
+// async function main() {
+//     await mongoose.connect('mongodb://127.0.0.1:27017/wanderlust');
+// }
+// main().then(()=>{
+//     console.log("connected to DB");
+// }).catch((err)=>{
+//     console.log(err);
+// })
 
-const initDB = async ()=>{
+module.exports.initDB = async ()=>{
     await Listing.deleteMany({});
     initData.data = initData.data.map((obj)=>({...obj,owner:"66b0f3ef4e734a5e94baac5a"}));
     const dataWithGeometry = await Promise.all(initData.data.map(async (d) => {
@@ -30,5 +30,3 @@ const initDB = async ()=>{
     await Listing.insertMany(dataWithGeometry);
     console.log("index connected");
 };
-
-initDB();
