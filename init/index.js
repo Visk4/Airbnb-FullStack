0ed560e193c5f6ae8e6551f6022f4560f1be8eb9
Visk4 <144266579+Visk4@users.noreply.github.com>
2024-08-17 17:41:05 +0530

    Update index.js

--- init/index.js
+++ init/index.js
@@ -12,7 +12,6 @@ const initData = require("./data.js");
 // })
 
 const initDB = async ()=>{
-    await Listing.deleteMany({});
     initData.data = initData.data.map((obj)=>({...obj,owner:"66c04b5836f8af22b8f0ad62"}));
     const dataWithGeometry = await Promise.all(initData.data.map(async (d) => {
         const response = await fetch(`https://api.geoapify.com/v1/geocode/search?text=${encodeURIComponent(d.location)}&apiKey=ee1f4c9d83e74f6bbe5c75a9b31ef7b2`);
